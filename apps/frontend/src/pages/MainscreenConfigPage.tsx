29091c0c38292f3e42e0bfd7cdf128ebda062cad
Janis M. Baumann <janis.baumann@eversys.com>
2025-06-05 14:42:29 +0200

    mainscreen

---
+++ apps/frontend/src/pages/MainscreenConfigPage.tsx
@@ -0,0 +1,6 @@
+import { Component } from "solid-js";
+
+export const MainscreenConfigPage: Component<{}> = () => {
+
+    return (<>Config</>);
+}
--- apps/frontend/src/pages/menu/navigationElements.tsx
+++ apps/frontend/src/pages/menu/navigationElements.tsx
@@ -2,6 +2,7 @@ import { Component } from 'solid-js';
 import { T } from '../../generated/language-types';
 import { EditScalesPage } from '@micra-pro/scale-management/feature';
 import { EditBeansPage } from '@micra-pro/bean-management/feature';
+import { MainscreenConfigPage } from '../MainscreenConfigPage';
 
 export const navigationElements: {
   link: string;
@@ -10,16 +11,22 @@ export const navigationElements: {
   component: Component;
 }[] = [
   {
-    name: () => <T key={'menu-sidebar-scales'} />,
-    link: 'scales',
-    icon: 'scale',
-    component: EditScalesPage,
+    name: () => <T key={'menu-sidebar-mainscreen-config'} />,
+    link: 'mainscreen-config',
+    icon: 'grid_view',
+    component: MainscreenConfigPage,
   },
   {
     name: () => <T key={'menu-sidebar-beans'} />,
     link: 'beans',
     icon: 'local_cafe',
     component: EditBeansPage,
+  },
+  {
+    name: () => <T key={'menu-sidebar-scales'} />,
+    link: 'scales',
+    icon: 'scale',
+    component: EditScalesPage,
   },
 ];
 
