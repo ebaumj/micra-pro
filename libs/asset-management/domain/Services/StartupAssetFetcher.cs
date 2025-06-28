4d3d9996da2ff86bafa9c805110b3e24af4aca7e
Janis M. Baumann <106016495+ebaumj@users.noreply.github.com>
2025-06-28 12:46:59 +0200

    repeat initial read of assets if it failed the first time

--- libs/asset-management/domain/Services/StartupAssetFetcher.cs
+++ libs/asset-management/domain/Services/StartupAssetFetcher.cs
@@ -1,3 +1,4 @@
+using System.Reactive.Linq;
 using MicraPro.AssetManagement.Domain.AssetAccess;
 using MicraPro.AssetManagement.Domain.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,12 +15,24 @@ public class StartupAssetFetcher(
     {
         await assetDirectoryService.ReadFilesAsync(cancellationToken);
         var serviceProvider = serviceScopeFactory.CreateScope().ServiceProvider;
-        await serviceProvider
+        var success = await serviceProvider
             .GetRequiredService<IAssetManagementService>()
             .SyncAssets(cancellationToken);
         await serviceProvider
             .GetRequiredService<IAssetCleaner>()
             .CleanupAssetsAsync(cancellationToken);
+        if (!success)
+            Observable
+                .Timer(TimeSpan.FromSeconds(3))
+                .Select(_ =>
+                    Observable.FromAsync(async ct =>
+                        await serviceProvider
+                            .GetRequiredService<IAssetManagementService>()
+                            .SyncAssets(ct)
+                    )
+                )
+                .Merge()
+                .Subscribe(_ => { });
     }
 
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
