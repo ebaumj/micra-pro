d89d3e485b3d2d8cd8c1e89f4ad70d91a7fd2500
Janis M. Baumann <106016495+ebaumj@users.noreply.github.com>
2025-08-26 17:20:10 +0200

    no error logs for non existing configs

--- libs/shared/data-provider-graphql/ConfigurationQueries.cs
+++ libs/shared/data-provider-graphql/ConfigurationQueries.cs
@@ -7,9 +7,19 @@ namespace MicraPro.Shared.DataProviderGraphQl;
 public static class ConfigurationQueries
 {
     [RequiredPermissions([Permission.ReadConfiguration])]
-    public static Task<string> ReadConfiguration(
+    public static async Task<string> ReadConfiguration(
         [Service] IConfigurationRepository service,
         string key,
         CancellationToken ct
-    ) => service.GetAsync(key, ct);
+    )
+    {
+        try
+        {
+            return await service.GetAsync(key, ct);
+        }
+        catch (InvalidOperationException)
+        {
+            throw new ConfigDoesNotExistException(key);
+        }
+    }
 }
