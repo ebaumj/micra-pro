3f715647b93e9bdf8fc84d031077f9a4bb787613
Janis M. Baumann <106016495+ebaumj@users.noreply.github.com>
2025-11-14 13:38:17 +0100

    remove scale id from history table

---
+++ libs/brew-by-weight/infrastructure/Migrations/20251114123029_RemoveScaleFromHistory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicraPro.BrewByWeight.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveScaleFromHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "ScaleId", table: "ProcessEntries");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ScaleId",
+                table: "ProcessEntries",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000")
+            );
+        }
+    }
+}
