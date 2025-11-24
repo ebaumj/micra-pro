d38b6f16a2612f71a0e0dc7354253a5074161dd7
Janis M. Baumann <106016495+ebaumj@users.noreply.github.com>
2025-11-24 10:01:14 +0100

    loading spinners not as dialog

--- apps/frontend/src/pages/MainscreenConfigPage.tsx
+++ apps/frontend/src/pages/MainscreenConfigPage.tsx
@@ -4,14 +4,7 @@ import {
   Roastery,
 } from '@micra-pro/bean-management/data-access';
 import { BeanSelectorDialog } from '@micra-pro/bean-management/feature';
-import {
-  AlertDialog,
-  AlertDialogContent,
-  CountryFlag,
-  Icon,
-  LongPressDiv,
-  Spinner,
-} from '@micra-pro/shared/ui';
+import { CountryFlag, Icon, LongPressDiv, Spinner } from '@micra-pro/shared/ui';
 import { createConfigAccessor } from '@micra-pro/shared/utils-ts';
 import { trackStore } from '@solid-primitives/deep';
 import { Component, createEffect, createSignal, For, on, Show } from 'solid-js';
@@ -130,74 +123,76 @@ export const MainscreenConfigPage: Component<{}> = () => {
     config.buttons.find((b) => b.pos.x === x && b.pos.y === y)?.beanId;
   return (
     <div class="flex h-full w-full flex-col">
-      <AlertDialog open={isLoading()}>
-        <AlertDialogContent class="flex items-center justify-center p-8">
-          <Spinner class="h-20 w-20" />
-        </AlertDialogContent>
-      </AlertDialog>
       <BeanSelectorDialog
         isOpen={!!currentPosition()}
         onBeanSelected={addOrUpdate}
         onClose={() => setCurrentPosition(undefined)}
         roasteries={roasteries.roasteries()}
       />
-      <div class="flex h-full items-center">
-        <div
-          class="grid w-full gap-2"
-          style={{
-            'grid-template-columns': `repeat(${config.numberOfButtonsColumns}, minmax(0, 1fr))`,
-            'grid-template-rows': `repeat(${config.numberOfButtonRows}, minmax(0, 1fr))`,
-          }}
-        >
-          <For each={[...Array(config.numberOfButtonsColumns).keys()]}>
-            {(x) => (
-              <For each={[...Array(config.numberOfButtonRows).keys()]}>
-                {(y) => (
-                  <div
-                    class="grid items-center before:col-start-1 before:col-end-2 before:row-start-1 before:row-end-2 before:block before:pb-[100%]"
-                    style={{
-                      'grid-row-start': y + 1,
-                      'grid-column-start': x + 1,
-                    }}
-                  >
-                    <div class="col-start-1 col-end-2 row-start-1 row-end-2 h-full">
-                      <LongPressDiv
-                        class={twMerge(
-                          'flex h-full w-full items-center justify-center rounded-lg border',
-                          beanId(x, y)
-                            ? 'bg-card text-card-foreground shadow-sm'
-                            : 'border-dashed',
-                        )}
-                        onClick={() => setCurrentPosition({ x, y })}
-                        onLongPress={() => tileLongPress(x, y)}
-                        onPressStart={() => setDeleting(true)}
-                        onPressEnd={() => setDeleting(false)}
-                        delayTimeMs={1000}
-                        maxShortPressTimeMs={300}
-                      >
-                        <Show when={beanId(x, y)}>
-                          {(b) => (
-                            <BeanPreview
-                              beanId={b()}
-                              roasteries={roasteries.roasteries()}
-                            />
-                          )}
-                        </Show>
-                      </LongPressDiv>
-                    </div>
-                  </div>
-                )}
-              </For>
-            )}
-          </For>
+      <Show when={isLoading()}>
+        <div class="flex h-full w-full items-center justify-center">
+          <Spinner class="h-20 w-20" />
         </div>
-      </div>
-      <div class="flex h-12 w-full items-center justify-end gap-2 px-2">
-        <Show when={deleting()}>
-          <Icon iconName="delete" class="text-destructive" />
-          <div class="animate-spin-loader-1s bg-destructive h-6 w-6 rotate-45 rounded-full" />
-        </Show>
-      </div>
+      </Show>
+      <Show when={!isLoading()}>
+        <div class="flex h-full items-center">
+          <div
+            class="grid w-full gap-2"
+            style={{
+              'grid-template-columns': `repeat(${config.numberOfButtonsColumns}, minmax(0, 1fr))`,
+              'grid-template-rows': `repeat(${config.numberOfButtonRows}, minmax(0, 1fr))`,
+            }}
+          >
+            <For each={[...Array(config.numberOfButtonsColumns).keys()]}>
+              {(x) => (
+                <For each={[...Array(config.numberOfButtonRows).keys()]}>
+                  {(y) => (
+                    <div
+                      class="grid items-center before:col-start-1 before:col-end-2 before:row-start-1 before:row-end-2 before:block before:pb-[100%]"
+                      style={{
+                        'grid-row-start': y + 1,
+                        'grid-column-start': x + 1,
+                      }}
+                    >
+                      <div class="col-start-1 col-end-2 row-start-1 row-end-2 h-full">
+                        <LongPressDiv
+                          class={twMerge(
+                            'flex h-full w-full items-center justify-center rounded-lg border',
+                            beanId(x, y)
+                              ? 'bg-card text-card-foreground shadow-sm'
+                              : 'border-dashed',
+                          )}
+                          onClick={() => setCurrentPosition({ x, y })}
+                          onLongPress={() => tileLongPress(x, y)}
+                          onPressStart={() => setDeleting(true)}
+                          onPressEnd={() => setDeleting(false)}
+                          delayTimeMs={1000}
+                          maxShortPressTimeMs={300}
+                        >
+                          <Show when={beanId(x, y)}>
+                            {(b) => (
+                              <BeanPreview
+                                beanId={b()}
+                                roasteries={roasteries.roasteries()}
+                              />
+                            )}
+                          </Show>
+                        </LongPressDiv>
+                      </div>
+                    </div>
+                  )}
+                </For>
+              )}
+            </For>
+          </div>
+        </div>
+        <div class="flex h-12 w-full items-center justify-end gap-2 px-2">
+          <Show when={deleting()}>
+            <Icon iconName="delete" class="text-destructive" />
+            <div class="animate-spin-loader-1s bg-destructive h-6 w-6 rotate-45 rounded-full" />
+          </Show>
+        </div>
+      </Show>
     </div>
   );
 };
