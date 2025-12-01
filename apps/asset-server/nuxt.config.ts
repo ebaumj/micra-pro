ec0cefa9117b23a135323b8e1da4ae39d8b0213d
Janis M. Baumann <janis.baumann@eversys.com>
2025-12-01 12:02:16 +0100

    use asset server token from environment

--- apps/asset-server/nuxt.config.ts
+++ apps/asset-server/nuxt.config.ts
@@ -8,9 +8,6 @@ export default defineNuxtConfig({
   },
   css: ['./app/assets/css/main.css'],
   runtimeConfig: {
-    secrets: {
-      privateKey: process.env.REMOTE_ASSET_SERVER_PRIVATE_KEY,
-    },
     authorization: {
       jwtAudience: 'MicraProAssetServer',
       jwtValidIssuers: ['MicraPro'],
